bb8db9d9a178a618a517648ca5e43a70bb3efaf9
Fadil Malik <55267669+fadilmalik@users.noreply.github.com>
2021-10-08 20:03:07 +0700

    commit

---
+++ main.js
@@ -0,0 +1,42 @@
+//onLoad
+fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(
+        function(response) {
+            if (response.status != 200) {
+                console.log(`Oopss... ${response.status}`)
+                return
+            }
+
+            response.json().then(function(data) {
+                const pokemons = data.results
+                pokemons.forEach(pokemon => {
+                    document.getElementById('pokemonList')
+                        .insertAdjacentHTML('beforeend',
+                            `<li onclick='detail("${pokemon.url}")'> ${pokemon.name} </li>`
+                        )
+                })
+            })
+        }
+    )
+    .catch(function(err) {
+        console.log(err)
+    })
+
+function detail(url) {
+    fetch(url).then(function(response) {
+        response.json().then(function(pokemon) {
+
+            document.getElementById('detail').innerHTML = ''
+
+            document.getElementById('detail')
+                .insertAdjacentHTML('beforeend',
+                    `<h3> ${pokemon.name} </h3>
+                <img src='${pokemon.sprites.front_default}'>
+                <p> Jurus: ${pokemon.moves[0].move.name} </p>
+                <p> Tinggi: ${pokemon.height} </p>
+                <p> Berat: ${pokemon.weight} </p>
+                `
+                )
+        })
+    })
+}
